bc980bc4b5f3dd49b23c767cfcfaceb9b78488f2
drullandev <drullan.dev@gmail.com>
2022-10-02 21:52:37 +0200

    New functions: login

--- strapi/config/admin.js
+++ strapi/config/admin.js
@@ -1,4 +1,7 @@
 module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env('API_TOKEN_SALT', '9gmek21dTpg6Q1iKhmvovQ=='),
+  },
   auth: {
     secret: env('ADMIN_JWT_SECRET', '6b6bbc74bfb36b525630e9c1b61527d1'),
   },
