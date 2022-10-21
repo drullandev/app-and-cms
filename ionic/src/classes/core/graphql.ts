c1a375ce97942a7744f172808d4c6e6be57fbe59
drullandev <drullan.dev@gmail.com>
2022-10-21 20:30:39 +0200

    Replacing with the new development

--- ionic/src/classes/core/graphql.ts
+++ ionic/src/classes/core/graphql.ts
@@ -1,4 +1,4 @@
-import { restCall, restCallAsync } from '../core/axios'
+import { restCall, restCallAsync } from './axios'
 import { AxiosRequestConfig } from 'axios'
 import { empty, camelCase } from '../common'
 import i18n from 'i18next'
